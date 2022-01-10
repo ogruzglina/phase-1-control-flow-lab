a40837a5722e3655e81ff4ce32854d831d20ee56
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-09 21:23:15 -0500

    commit tree b640bdbc9ff4a0dcaaea7c3dd9a00627d616669a

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500){
+    return 'No can do.';
+  }
 }
+console.log(scuberGreetingForFeet(25));
+console.log(scuberGreetingForFeet(500)); //undefined bc we don't have condition between 401 and 1999 feet
+console.log(scuberGreetingForFeet(2500));
+console.log(scuberGreetingForFeet(12500));
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('Dnipro'));
+console.log(ternaryCheckCity(123));
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous': 
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default: 
+      return 'Bye.';
+      break;
+  }
 }
+console.log(switchOnCharmFromTip('generous'));
+console.log(switchOnCharmFromTip('not as generous'));
+console.log(switchOnCharmFromTip('thank you'));
